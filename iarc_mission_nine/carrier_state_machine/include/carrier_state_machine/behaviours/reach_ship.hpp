5eee4c27fb18b4a971fac8a21cd5378e1a3223dd
rohanblueboybaijal <rohanbaijal2702@gmail.com>
2021-01-06 12:18:59 +0530

    Integrate rosplane code for fixed wing trajectory

--- iarc_mission_nine/carrier_state_machine/include/carrier_state_machine/behaviours/reach_ship.hpp
+++ iarc_mission_nine/carrier_state_machine/include/carrier_state_machine/behaviours/reach_ship.hpp
@@ -2,6 +2,8 @@
 
 #include <carrier_state_machine/carrier_state/carrier_state.hpp>
 
+//#include <path_planner.h>  for rosplane 
+
 namespace ariitk::carrier_state_machine {
 
 class ReachShip {
@@ -11,8 +13,10 @@ class ReachShip {
     void execute(const Event& evt);
     void init(ros::NodeHandle nh, ros::NodeHandle nh_private, const std::shared_ptr<CarrierState> state_ptr);
 
+
   private:
     std::shared_ptr<CarrierState> mav_state_;
+    RosplaneDubins rosplane_;
 };
 
 }  // namespace ariitk::carrier_state_machine

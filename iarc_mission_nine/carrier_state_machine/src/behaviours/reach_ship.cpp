f2fa430c51e26d28ab758c4839698f2c7f8c5e54
kpiyush25 <piyushk@iitk.ac.in>
2021-01-15 23:30:38 +0530

    Add function call for forward journey

--- iarc_mission_nine/carrier_state_machine/src/behaviours/reach_ship.cpp
+++ iarc_mission_nine/carrier_state_machine/src/behaviours/reach_ship.cpp
@@ -3,11 +3,10 @@
 namespace ariitk::carrier_state_machine {
 
 void ReachShip::init(ros::NodeHandle nh, ros::NodeHandle nh_private, const std::shared_ptr<CarrierState> state_ptr) {
-    rosplane_.init(nh);
 }
 
 void ReachShip::execute(const Event& evt) {
-    rosplane_.run();
+    est->forwardRun();
 }
 
 }  // namespace ariitk::carrier_state_machine

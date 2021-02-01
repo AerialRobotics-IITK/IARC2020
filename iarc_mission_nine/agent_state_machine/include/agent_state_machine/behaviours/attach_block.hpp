7ef393f1061e887a20e9b9818d62501fd04bf300
AtharvaUmbarkar <atharvau@iitk.ac.in>
2021-02-02 02:08:36 +0530

    Add Attach_Block Service

--- iarc_mission_nine/agent_state_machine/include/agent_state_machine/behaviours/attach_block.hpp
+++ iarc_mission_nine/agent_state_machine/include/agent_state_machine/behaviours/attach_block.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <agent_state_machine/agent_state/agent_state.hpp>
+#include "gazebo_ros_link_attacher/Attach.h"          //! Paths are mp incorrect
+#include "gazebo_ros_link_attacher/AttachRequest.h"
+#include "gazebo_ros_link_attacher/AttachResponse.h"
 
 namespace ariitk::agent_state_machine {
 
@@ -13,6 +16,8 @@ class AttachBlock {
 
   private:
     std::shared_ptr<AgentState> mav_state_;
+    ros::ServiceClient attach_client_;
+    gazebo_ros_link_attacher::AttachRequest req_;    //! Be wary of Namespace
 };
 
 }  // namespace ariitk::agent_state_machine
--- iarc_mission_nine/agent_state_machine/src/behaviours/attach_block.cpp
+++ iarc_mission_nine/agent_state_machine/src/behaviours/attach_block.cpp
@@ -3,6 +3,14 @@
 namespace ariitk::agent_state_machine {
 
 void AttachBlock::init(ros::NodeHandle& nh, ros::NodeHandle& nh_private, const std::shared_ptr<AgentState> state_ptr) {
+    attach_client_ = nh.ServiceClient<gazebo_ros_link_attacher::AttacherRequest>("link_attacher_node/attach");
+
+    bool flag = false;
+
+    req_.model_name_1 = "gripper";
+    req_.link_name_1 "left_short_joint";
+    req_.model_name_2 = "communications_module";
+    req_.link_name_2 = "link";
 }
 
 void AttachBlock::execute(const Event evt) {
@@ -10,6 +18,10 @@ void AttachBlock::execute(const Event evt) {
     ros::Rate loop_rate(30.0);
     while (ros::ok()) {
         ros::spinOnce();
+        if (flag == false) {
+            attach_client_.call(req_);
+            flag = true;
+        }
         loop_rate.sleep();
     }
 }

849bba6dade556c2421a23912bd5ef9a589d14e5
kpiyush25 <piyushk@iitk.ac.in>
2021-02-28 16:39:40 +0530

    Add forwardModeOn function

--- iarc_mission_nine/carrier_state_machine/src/behaviours/reach_ship.cpp
+++ iarc_mission_nine/carrier_state_machine/src/behaviours/reach_ship.cpp
@@ -4,20 +4,18 @@ namespace ariitk::carrier_state_machine {
 
 void ReachShip::init(ros::NodeHandle nh, ros::NodeHandle nh_private, const std::shared_ptr<CarrierState> state_ptr) {
     ROS_ERROR("reachship init called");
-
-    ROS_ERROR("reachship init executed completely");
 }
 
 void ReachShip::execute(const Event& evt) {
-    rosplane::path_manager_base* est = new rosplane::path_manager_example();
-    est->forwardRun();
-    // ros::Rate looprate(10);
-    // while (ros::ok()) {
-    //     looprate.sleep();
+    ROS_ERROR("reachship execute called");
+    rosplane::path_manager_example* it = new rosplane::path_manager_example();
+    it->forwardModeOn();  // It makes the boolean forward_ true which is to be used in path_manager_example.cpp to call forwardRun().
 
-    //     ros::spinOnce();
-    // }
+    rosplane::path_manager_base* est = new rosplane::path_manager_example();  // Assigned the pointer of path_manager_example to the pointer of
+                                                                              // path_manager_base as the former is a class inherited from the latter one.
+    est->forwardRun();
     ros::spin();
+    ROS_ERROR("reachship execute successfully executed");
 }
 
 }  // namespace ariitk::carrier_state_machine
